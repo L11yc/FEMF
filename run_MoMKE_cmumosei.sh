7736d951f8f235d9fb5333531db716cc1363cad9
L11yc <lyc@stu.cqut.edu.cn>
2025-10-20 10:58:32 +0800

    Update run_MoMKE_cmumosei.sh

--- run_MoMKE_cmumosei.sh
+++ run_MoMKE_cmumosei.sh
@@ -7,7 +7,7 @@
 # python -u MoMKE/train_MoMKE.py --dataset=CMUMOSEI --audio-feature=wav2vec-large-c-UTT --text-feature=deberta-large-4-UTT --video-feature=manet_UTT --seed=66 --batch-size=32 --epoch=100 --lr=0.0001 --hidden=256 --depth=4 --num_heads=2 --drop_rate=0.5 --attn_drop_rate=0.0 --test_condition=av --stage_epoch=50 --gpu=0
 # python -u MoMKE/train_MoMKE.py --dataset=CMUMOSEI --audio-feature=wav2vec-large-c-UTT --text-feature=deberta-large-4-UTT --video-feature=manet_UTT --seed=66 --batch-size=32 --epoch=100 --lr=0.0001 --hidden=256 --depth=4 --num_heads=2 --drop_rate=0.5 --attn_drop_rate=0.0 --test_condition=tv --stage_epoch=50 --gpu=0
  
-python -u MoMKE/train_MoMKE.py --dataset=CMUMOSEI --audio-feature=wav2vec-large-c-UTT --text-feature=deberta-large-4-UTT --video-feature=manet_UTT --seed=67 --batch-size=64 --epoch=100 --lr=0.0001 --hidden=256 --depth=10 --num_heads=2 --drop_rate=0.4 --attn_drop_rate=0.0 --test_condition=atv --stage_epoch=50 --gpu=0
+python -u MoMKE/train_MoMKE.py --dataset=CMUMOSEI --audio-feature=wav2vec-large-c-UTT --text-feature=deberta-large-4-UTT --video-feature=manet_UTT --seed=67 --batch-size=64 --epoch=100 --lr=0.0001 --hidden=256 --depth=4 --num_heads=2 --drop_rate=0.4 --attn_drop_rate=0.0 --test_condition=atv --stage_epoch=50 --gpu=0
 # python -u MoMKE/train_MoMKE.py --dataset=CMUMOSEI --audio-feature=wav2vec-large-c-UTT --text-feature=deberta-large-4-UTT --video-feature=manet_UTT --seed=66 --batch-size=32 --epoch=100 --lr=0.0001 --hidden=256 --depth=4 --num_heads=2 --drop_rate=0.5 --attn_drop_rate=0.0 --test_condition=atv --stage_epoch=50 --gpu=0
 
 # w/o moe
